c0ea3b9a8eb824b1d5432d5cedb08d61a43f9750
yTpO <Lomako_Sergei@tut.by>
2022-11-03 11:09:53 +0300

    stylefix after review

--- Words/Words/Program.cs
+++ Words/Words/Program.cs
@@ -4,90 +4,81 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        bool exception = false;
         string path = "inputData.txt";
         var initialWordsList = new List<string>();
 
         try
         {
-            using StreamReader reader = new StreamReader(path);
+            using var reader = new StreamReader(path);
             string? line;
             while ((line = await reader.ReadLineAsync()) != null)
-            {
                 initialWordsList.Add(line);
-            }
         }
         catch (FileNotFoundException)
         {
             Console.WriteLine("Файл не найден");
-            exception = true;
+            return;
         }
 
         Console.Write("Введите время ввода слова (сек): ");
+
         int timeForReadline = Convert.ToInt32(Console.ReadLine());
         Console.Clear();
 
-        if (!exception)
+        Console.WriteLine("Выберите слово, с которым будете играть");
+
+        for (int i = 1; i <= initialWordsList.Count; i++)
+            Console.WriteLine($"{i}. {initialWordsList[i - 1]}");
+
+        string parentWord;
+        try
         {
-            Console.WriteLine("Выберите слово, с которым будете играть");
-            int i = 0;
-            foreach (string word in initialWordsList)
-            {
-                Console.WriteLine($"{++i}. {word}");
-            }
-            string parentWord = "";
-            try
-            {
-                int parentWordIndex = Convert.ToInt32(Console.ReadLine());
-                parentWord = initialWordsList[--parentWordIndex];
-            }
-            catch
-            {
-                Console.WriteLine("Возникла ошибка!");
-                exception = true;
-            }
+            int parentWordIndex = Convert.ToInt32(Console.ReadLine());
+            parentWord = initialWordsList[--parentWordIndex];
+        }
+        catch
+        {
+            Console.WriteLine("Возникла ошибка!");
+            return;
+        }
 
-            if (exception != true)
+        if (parentWord != "")
+        {
+            Console.Clear();
+            Console.WriteLine($"Вы выбрали слово '{parentWord}'\n");
+        }
+
+        bool gameOver = false;
+        var usedWords = new List<string>();
+
+        while (!gameOver)
+        {
+            DateTime t = DateTime.Now;
+
+            Console.Write("Введите слово: ");
+            string inputWord = Console.ReadLine();
+
+            if ((DateTime.Now - t).TotalSeconds < timeForReadline)
             {
-                if (parentWord != "")
+                if (CheckWords(inputWord, parentWord))
                 {
-                    Console.Clear();
-                    Console.WriteLine($"Вы выбрали слово '{parentWord}'\n");
+                    if (!ExistWords(inputWord, usedWords))
+                        gameOver = GameOverText("Такое слово уже вводилось!");
                 }
-
-                bool gameOver = false;
-                var usedWords = new List<string>();
-
-                while (!gameOver)
-                {
-                    DateTime t = DateTime.Now;
-
-                    Console.Write("Введите слово: ");
-                    string inputWord = Console.ReadLine();
-
-                    if ((DateTime.Now - t).TotalSeconds < timeForReadline)
-                    {
-                        if (checkWords(inputWord, parentWord))
-                        {
-                            if (!existWords(inputWord, usedWords))
-                                gameOver = gameOverText("Такое слово уже вводилось!");
-                        }
-                        else
-                            gameOver = gameOverText("Такое слово нельзя составить из родительского слова!");
-                    }
-                    else
-                        gameOver = gameOverText("Превышено время ожидания!");
-                }
+                else
+                    gameOver = GameOverText("Такое слово нельзя составить из родительского слова!");
             }
+            else
+                gameOver = GameOverText("Превышено время ожидания!");
         }
 
         Console.ReadKey();
     }
 
-    public static bool checkWords(string word1, string word2)
+    public static bool CheckWords(string newWord, string parentWord)
     {
-        var word2List = word2.ToList();
-        foreach (char ch in word1)
+        var word2List = parentWord.ToList();
+        foreach (char ch in newWord)
         {
             if (word2List.IndexOf(ch) < 0)
                 return false;
@@ -96,7 +87,7 @@ internal class Program
         return true;
     }
 
-    public static bool existWords(string newWord, List<string> usedWords)
+    public static bool ExistWords(string newWord, List<string> usedWords)
     {
         if (usedWords.IndexOf(newWord) < 0)
         {
@@ -107,7 +98,7 @@ internal class Program
             return false;
     }
 
-    public static bool gameOverText(string errorMessage)
+    public static bool GameOverText(string errorMessage)
     {
         Console.WriteLine($"\n{errorMessage}\nВы проиграли");
         return true;
